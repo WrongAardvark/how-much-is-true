6cb304a70de77207a1eee7aa21826fd80e079af6
Mark Dowell <30960903+WrongAardvark@users.noreply.github.com>
2024-10-04 09:27:08 -0500

    Create true.py

---
+++ true.py
@@ -0,0 +1,11 @@
+# Create a function which returns the number of true values there are in an array.
+
+
+def countTrue(array):
+    numTrue = array.count(True)
+    print(numTrue)
+
+
+countTrue(put your array here)
+
+# I'm assuming it's all booleans cause I'm lazy.
